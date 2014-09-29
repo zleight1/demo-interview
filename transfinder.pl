6b30462cb0824d6a02a86d47106e5f0626378ae0
Zachary Leighton <zleight1@binghamton.edu>
2014-09-29 18:43:47 -0400

    Extended API a bit

--- transfinder.pl
+++ transfinder.pl
@@ -13,26 +13,50 @@ get '/' => sub {
 get '/api/:object' => sub {
     my $c = shift;
 
+    my $url = $c->stash('object');
+    $url .= makeParams($c);
+
+    $c->render( text => apiProxy($url) );   
+
+};
+
+get '/api/:object/:id' => sub {
+    my $c = shift;
+
+    my $url = $c->stash('object') . "/" . $c->stash('id');
+    $url .= makeParams($c);
+
+    $c->render( text => apiProxy($url) );
+};
+
+get '/api/:object/:id/:subobject' => sub {
+    my $c = shift;
+
+    my $url = $c->stash('object') . "/" . $c->stash('id') . "/" . $c->stash('subobject');
+    $url .= makeParams($c);
+
+    $c->render( text => apiProxy($url) );
+};
+
+sub apiProxy {
+    my $url = shift;
+
     my $ua = LWP::UserAgent->new;
     $ua->timeout(10);
     $ua->env_proxy;
     $ua->default_header(
         'Authorization' => "Basic emxlaWdodG9uOnpsZWlnaHRvbjEyMw==" );
 
-    my $url = $c->stash('object');
-    $url .= makeParams($c);
-
-    my $response = $ua->get(
+   my $response = $ua->get(
         'http://50.19.85.246/devsample/apiv1.svc/databases/1/' . $url );
 
-    if ( $response->is_success ) {
-        $c->render( text => $response->decoded_content );    # or whatever
+   if ( $response->is_success ) {
+        return $response->decoded_content;
     }
     else {
         die $response->status_line;
     }
-
-};
+}
 
 sub makeParams {
     my $c      = shift;
